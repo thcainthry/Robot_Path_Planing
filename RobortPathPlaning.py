5b39ea8044e2af68900473e4556d29d30f443b90
Alma-L <alma.latifi1@student.uni-pr.edu>
2023-05-15 21:07:19 +0200

    Update: Implement Spot class and define colors for pathfinding visualization This commit introduces the Spot class, which represents a spot or node in the pathfinding algorithm. Each spot has properties such as row, column, coordinates, color, neighbors, width, and total rows. Additionally, the commit defines color constants to be used for visualization purposes.

--- RobortPathPlaning.py
+++ RobortPathPlaning.py
@@ -1,34 +1,29 @@
-try:
-    import pygame
-    import sys
-    import math
-    from tkinter import *
-    from tkinter import ttk
-    from tkinter import messagebox
-    import os
-except:
-    import install_requirements  # install packages
+import pygame
+import math
+from queue import PriorityQueue
 
-    import pygame
-    import sys
-    import math
-    from tkinter import *
-    from tkinter import ttk
-    from tkinter import messagebox
-    import os
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("A* Path Finding Algorithm")
 
-screen = pygame.display.set_mode((800, 800))
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165 ,0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
 
-class spot:
-
-    def __init__(self, x, y):
-        self.i = x
-        self.j = y
-        self.f = 0
-        self.g = 0
-        self.h = 0
+class Spot:
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
         self.neighbors = []
-        self.previous = None
-        self.obs = False
-        self.closed = False
-        self.value = 1
+        self.width = width
+        self.total_rows = total_rows
