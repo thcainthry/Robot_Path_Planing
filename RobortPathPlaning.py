0efadf56da2921d1f80183753da0e93ca84ecda1
DoresaO <doresa.osmanaj@student.uni-pr.edu>
2023-05-17 11:34:21 +0200

    Pathfinding Functions

--- RobortPathPlaning.py
+++ RobortPathPlaning.py
@@ -86,3 +86,17 @@ class Spot:
 
 	def __lt__(self, other):
 		return False
+    
+
+def h(p1, p2):
+	x1, y1 = p1
+	x2, y2 = p2
+	return abs(x1 - x2) + abs(y1 - y2)
+
+
+
+def reconstruct_path(came_from, current, draw):
+	while current in came_from:
+		current = came_from[current]
+		current.make_path()
+		draw()
