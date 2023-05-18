09a9175eacd5bdcaee859aa9f731ec16f3616f63
aritaazizi1 <arita.azizi@student.uni-pr.edu>
2023-05-18 14:18:15 +0200

    Implement a Grid Drawing Function

--- RobortPathPlaning.py
+++ RobortPathPlaning.py
@@ -164,3 +164,24 @@ def draw_grid(win, rows, width):
         pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
         for j in range(rows):
             pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
+	    
+
+def draw(win, grid, rows, width):
+	win.fill(WHITE)
+
+	for row in grid:
+		for spot in row:
+			spot.draw(win)
+
+	draw_grid(win, rows, width)
+	pygame.display.update()
+
+
+def get_clicked_pos(pos, rows, width):
+	gap = width // rows
+	y, x = pos
+
+	row = y // gap
+	col = x // gap
+
+	return row, col
