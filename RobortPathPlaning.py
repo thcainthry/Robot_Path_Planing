cea32e658ce50b7a5618373762557ce85a0a6b2e
HMuhamed <50260119+HMuhamed@users.noreply.github.com>
2023-05-18 10:23:12 +0200

    Update RobortPathPlaning.py

--- RobortPathPlaning.py
+++ RobortPathPlaning.py
@@ -100,3 +100,23 @@ def reconstruct_path(came_from, current, draw):
 		current = came_from[current]
 		current.make_path()
 		draw()
+
+		def algorithm(draw, grid, start, end):
+	count = 0
+	open_set = PriorityQueue()
+	open_set.put((0, count, start))
+	came_from = {}
+	g_score = {spot: float("inf") for row in grid for spot in row}
+	g_score[start] = 0
+	f_score = {spot: float("inf") for row in grid for spot in row}
+	f_score[start] = h(start.get_pos(), end.get_pos())
+
+	open_set_hash = {start}
+
+	while not open_set.empty():
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+
+		current = open_set.get()[2]
+		open_set_hash.remove(current)
